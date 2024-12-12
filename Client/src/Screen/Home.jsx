968b9de16ed753880b70280c1edf93339d8fbab3
talhahzahid <talhahzahid415@gmail.com>
2024-12-12 18:30:57 +0500

    Working on pages

---
+++ Client/src/Screen/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h1 className="text-center mt-7 font-semibold text-xl">
+        ALL BLOG ARE HERE
+      </h1>
+    </>
+  );
+};
+
+export default Home;
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// index.js
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Layout from "../Layout";
+import Home from "./Screen/Home";
+import AddBlog from "./Screen/AddBlog";
+import Signup from "./Screen/Signup";
+import SignIn from "./Screen/SignIn";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "addblog",
+        element: <AddBlog />,
+      },
+      {
+        path: "signup",
+        element: <Signup />,
+      },
+      {
+        path: "signin",
+        element: <SignIn />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
